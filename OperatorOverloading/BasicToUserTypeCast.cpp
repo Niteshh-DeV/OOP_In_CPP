2af7568dfa93e44e3c73d91cce05aadd25373757
Nitesh Joshi <niteshjoshi010@gmail.com>
2024-09-01 21:35:56 +0545

    Basiic To User Defined Data type conversion

---
+++ OperatorOverloading/BasicToUserTypeCast.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Time
+{
+    int hh,mm,ss;
+   public: Time ()
+ {
+    hh=0;
+    mm=0;
+    ss=0;
+
+ }  
+ Time(int s){
+    hh=s/3600;
+    s=s%60;
+    mm=s/60;
+    ss=s%60;
+
+ } 
+ void show ()
+ {
+   cout<<hh<<":"<<mm<<":"<<ss;
+ }
+};
+
+
+int main()
+{
+   Time t1,t2;
+   t1=207040;
+   t2=308031;
+    cout<<"time t1:\t";t1.show(); 
+    cout<<endl;
+    cout<<"time t2:\t";t2.show();
+    return 0;
+}
