8ad2ce3f5754f5d93922eb46786f6b21f397032f
Nitesh Joshi <niteshjoshi010@gmail.com>
2024-09-03 22:27:01 +0545

    Example of Unary Prefix and unary postfix operator

--- OperatorOverloading/UnaryOperatorOverloading.cpp
+++ OperatorOverloading/UnaryOperatorOverloading.cpp
@@ -1,3 +1,36 @@
+// #include <iostream>
+// using namespace std;
+// class Unary
+// {
+//     int count ;
+//     public:
+//             Unary()
+//             {
+//                 count = 0 ;
+//             }
+//     void  operator ++ ()
+//     {
+//         ++count ;
+//     }
+//     void display()
+//     {
+//         cout<<count;
+//     }
+// };
+
+// int main() {
+//     Unary obj1 , obj2;
+//     cout << "This Is Example of  Unary prefix Operator" << endl;
+//     ++obj1;
+//     ++obj2;
+//     ++obj2;
+//     cout << "Count of obj1 is " ;obj1.display() ;
+//     cout << "\n Count of obj2 is " ;obj2.display() ;
+//     return 0;
+// }
+ //Example of  Unary Postfix Operator
+
+
 #include <iostream>
 using namespace std;
 class Unary
@@ -8,9 +41,9 @@ class Unary
             {
                 count = 0 ;
             }
-    void  operator ++ ()
+    void  operator ++ (int)
     {
-        ++count ;
+       count++;
     }
     void display()
     {
@@ -20,10 +53,10 @@ class Unary
 
 int main() {
     Unary obj1 , obj2;
-    cout << "This Is Example of  Unary prefix Operator" << endl;
-    ++obj1;
-    ++obj2;
-    ++obj2;
+    cout << "This Is Example of  Unary postfix Operator" << endl;
+    obj1++;
+    obj2++;
+    obj2++;
     cout << "Count of obj1 is " ;obj1.display() ;
     cout << "\n Count of obj2 is " ;obj2.display() ;
     return 0;
