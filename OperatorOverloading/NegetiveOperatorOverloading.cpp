aa7d61b54faea08d8ec3cad824383ee4dfb9e2ca
Nitesh Joshi <niteshjoshi010@gmail.com>
2024-09-05 09:15:58 +0545

    -ve operator overloading in c++

--- OperatorOverloading/NegetiveOperatorOverloading.cpp
+++ OperatorOverloading/NegetiveOperatorOverloading.cpp
@@ -1,3 +1,54 @@
+// #include <iostream>
+// using namespace std;
+
+// class space
+// {
+//     int x,y,z ;
+//     public:
+//         space()
+//         {
+//             x=0;
+//             y=0;
+//             z=0 ;
+//         }
+//         space(int ,int , int );
+//         void display();
+//         void operator -();
+// };
+
+// space :: space(int a , int b , int c)
+// {
+//     x=a ;
+//     y=b ;
+//     z=c ;
+// }
+
+// void space :: display()
+// {
+//     cout<<x <<"\t"<< y<<"\t"<<z ;
+
+// }
+// void  space :: operator -()
+// {
+//     x=-x;
+//     y=-y;
+//     z=-z;
+// }
+
+
+// int main() {
+//     cout << "This is a example of - operator overloading" <<endl;
+//     space s2 ,s1(-10,20,-30);
+
+//     cout<<" s1 =";s1.display();
+//     -s1 ;
+//     cout<<"\n -s1 =";s1.display();
+    
+
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
@@ -13,7 +64,7 @@ class space
         }
         space(int ,int , int );
         void display();
-        void operator -();
+      friend void operator -(space &);
 };
 
 space :: space(int a , int b , int c)
@@ -28,11 +79,11 @@ void space :: display()
     cout<<x <<"\t"<< y<<"\t"<<z ;
 
 }
-void  space :: operator -()
+void operator -(space &s)
 {
-    x=-x;
-    y=-y;
-    z=-z;
+    s.x=-s.x;
+    s.y=-s.y;
+    s.z=-s.z;
 }
 
 
